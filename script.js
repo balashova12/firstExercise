93a3ce1cbf0aa6a69c428aa7479351bf87966a7a
Aleksandra Balashova <ealba003@edu.xamk.fi>
2024-01-13 20:00:16 +0200

    final commit

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
-document.getElementById("rightanswer").addEventListener("click",
- function() {
-    alert("Correct!!!");
-}
-);
-document.getElementById("rightanswer").addEventListener("click",
- function() {
-    alert("Try again");
-}
-);
+document.getElementById("actionButton").addEventListener("click", function() {
+    alert("Button clicked!");
+});
 
+document.getElementById("year").innerHTML = new Date().getFullYear();
+
+document.getElementById("actionButton").addEventListener("click", function() {
+    document.getElementById("description").classList.toggle('nonvisible');
+});
